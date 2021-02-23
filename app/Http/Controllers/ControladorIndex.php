7abcf0e1ac67517b689b6d7d146fed3b78558aad
Naia <naia.idgalo@gmail.com>
2021-02-23 21:36:54 +0100

    pequeñosCambios

--- app/Http/Controllers/ControladorIndex.php
+++ app/Http/Controllers/ControladorIndex.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class ControladorIndex extends Controller
 {
-public function home(){
-    return redirect()->route("principales.index");
-}
+    public function home(){
+        return view("principales.index");
+    }
 }
