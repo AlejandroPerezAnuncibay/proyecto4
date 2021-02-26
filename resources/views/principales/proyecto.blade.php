d5d7a0115b2cd06c035edd6bb66815b23d10502c
AlejandroPerezAnuncibay <alejandro.perez@ikasle.egibide.org>
2021-02-26 19:12:23 +0100

    footer clonado eliminado

--- resources/views/principales/proyecto.blade.php
+++ resources/views/principales/proyecto.blade.php
@@ -958,24 +958,6 @@
             </div>
         </main>
 
-        <footer>
-            <div class="footy-sec mn no-margin">
-                <div class="container">
-                    <ul>
-                        <li><a href="#" title="">Help Center</a></li>
-                        <li><a href="#" title="">Privacy Policy</a></li>
-                        <li><a href="#" title="">Community Guidelines</a></li>
-                        <li><a href="#" title="">Cookies Policy</a></li>
-                        <li><a href="#" title="">Career</a></li>
-                        <li><a href="#" title="">Forum</a></li>
-                        <li><a href="#" title="">Language</a></li>
-                        <li><a href="#" title="">Copyright Policy</a></li>
-                    </ul>
-                    <p><img src="../../../../public/images/copy-icon2.png" alt="">Copyright 2018</p>
-                    <img class="fl-rgt" src="../../../../public/images/logo2.png" alt="">
-                </div>
-            </div>
-        </footer><!--footer end-->
 
         <div class="overview-box" id="overview-box">
             <div class="overview-edit">
