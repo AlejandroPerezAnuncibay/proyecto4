d6c56809322fd39b3e2863d9c454e0c79af5641d
AlejandroPerezAnuncibay <alejandro.perez@ikasle.egibide.org>
2021-02-26 21:32:55 +0100

    tablas arreglado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <meta name="description" content="" />
     <meta name="keywords" content="" />
     <link rel="stylesheet" type="text/css" href="{{ asset ('css/animate.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{ asset ('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="{{ asset ('css/flatpickr.min.css')}}">
     <link rel="stylesheet" type="text/css" href="{{ asset ('css/line-awesome.css')}}">
     <link rel="stylesheet" type="text/css" href="{{ asset ('css/line-awesome-font-awesome.min.css')}}">
@@ -132,7 +132,7 @@
 
 <script type="text/javascript" src="{{ asset ('js/jquery.min.js')}}"></script>
 <script type="text/javascript" src="{{ asset ('js/popper.js')}}"></script>
-<script type="text/javascript" src="{{ asset ('js/bootstrap.min.js')}}"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
 <script type="text/javascript" src="{{asset('js/tabla.js')}}"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.23/datatables.min.js"></script>
 <script type="text/javascript" src="{{ asset ('js/flatpickr.min.js')}}"></script>
