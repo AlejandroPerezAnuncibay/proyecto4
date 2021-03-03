a765e239272b5399bab9e7f27508e8ba2c461d74
AlejandroPerezAnuncibay <alejandro.perez@ikasle.egibide.org>
2021-03-03 10:40:26 +0100

    cambios graficas otra

--- resources/views/principales/estadisticas.blade.php
+++ resources/views/principales/estadisticas.blade.php
@@ -27,7 +27,7 @@
                 <div class="posts-section" id="graficos" style="display: block">
                 <ul class="nav nav-tabs" id="myTab" role="tablist">
                     <li class="nav-item">
-                        <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Proyectos</a>
+                        <a class="nav-link " id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Proyectos</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Mensajes</a>
@@ -39,7 +39,7 @@
                 </ul>
 
                 <div class="tab-content" id="tab-content" style="display: none;">
-                    <div class="tab-pane active" id="home" role="tabpanel" aria-labelledby="home-tab"><div id="chart"></div></div>
+                    <div class="tab-pane " id="home" role="tabpanel" aria-labelledby="home-tab"><div id="chart"></div></div>
                     <div class="tab-pane" id="profile" role="tabpanel" aria-labelledby="profile-tab"><div id="chart2"></div></div>
                     <div class="tab-pane" id="messages" role="tabpanel" aria-labelledby="messages-tab"><div id="chart3" ></div>
                 </div>
